63b2f50d86f5eee761d0124d0f50e8a48ed399e7
Edrei Barajas <edrei.barajas14@gmail.com>
2025-09-06 13:32:19 -0600

    Now the cart values are displayed

--- src/js/product.js
+++ src/js/product.js
@@ -4,10 +4,14 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
+  let cartItems = getLocalStorage("so-cart");
+  if (!Array.isArray(cartItems)) {
+    cartItems = [];
+  }
   cartItems.push(product);
   setLocalStorage("so-cart", cartItems);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
