a5d1f1cc8d515464f45aa4d490121c05d0f96050
Edrei Barajas <edrei.barajas14@gmail.com>
2025-09-13 14:43:17 -0600

    some more changes in js files

--- src/js/product.js
+++ src/js/product.js
@@ -2,10 +2,8 @@ import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-const productId = getParam("product");
-
 const dataSource = new ProductData("tents");
+const productID = getParam("product");
 
-const product = new ProductDetails(productId, dataSource);
-
+const product = new ProductDetails(productID, dataSource);
 product.init();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -22,6 +22,7 @@ export function setClick(selector, callback) {
   qs(selector).addEventListener("click", callback);
 }
 
+// get the product id from the query string
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
@@ -31,6 +32,7 @@ export function getParam(param) {
 
 export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
   const htmlStrings = list.map(template);
+  // if clear is true we need to clear out the contents of the parent.
   if (clear) {
     parentElement.innerHTML = "";
   }
