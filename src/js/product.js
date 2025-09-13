adec21b4567ec81d180ede35fb258984add16ade
Edrei Barajas <edrei.barajas14@gmail.com>
2025-09-13 14:37:42 -0600

    changes in some code lines

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,7 @@ import ProductDetails from "./ProductDetails.mjs";
 
 const productId = getParam("product");
 
-const dataSource = new ProductData("public/json/tents.json");
+const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
 
