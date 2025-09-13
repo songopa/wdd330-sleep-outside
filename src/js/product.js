9628b5771083cf055da89ccece079c402c02c018
Edrei Barajas <edrei.barajas14@gmail.com>
2025-09-12 20:40:59 -0600

    Last corrections

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,6 @@
 import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails from "./productDetails.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const productId = getParam("product");
 
@@ -9,13 +9,4 @@ const dataSource = new ProductData("json/tents.json");
 const product = new ProductDetails(productId, dataSource);
 
 product.init();
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
